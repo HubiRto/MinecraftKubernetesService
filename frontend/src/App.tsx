d73a6a574dd9beaa31a34db558413e6c4272f60d
hubirto <hubert.rybicki.hr@gmail.com>
2024-04-06 17:24:10 +0200

    Change javascript project module to typescript

---
+++ frontend/src/App.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import './App.css';
+import ServerPage from "./Pages/ServerPage/ServerPage";
+
+function App() {
+    return (
+        <ServerPage />
+    );
+}
+
+export default App;
