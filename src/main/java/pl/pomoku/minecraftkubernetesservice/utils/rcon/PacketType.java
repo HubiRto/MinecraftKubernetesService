5e532359205ae68f5b2dd05fd1674742b390549e
hubirto <hubert.rybicki.hr@gmail.com>
2024-04-16 16:35:00 +0200

    Add RCON Util

---
+++ src/main/java/pl/pomoku/minecraftkubernetesservice/utils/rcon/PacketType.java
@@ -0,0 +1,11 @@
+package pl.pomoku.minecraftkubernetesservice.utils.rcon;
+
+public class PacketType {
+
+    private PacketType() {}
+    
+    final public static int SERVERDATA_RESPONSE_VALUE = 0;
+    final public static int SERVERDATA_EXECCOMMAND = 2;
+    final public static int SERVERDATA_AUTH_RESPONSE = 2;
+    final public static int SERVERDATA_AUTH = 3;
+}
